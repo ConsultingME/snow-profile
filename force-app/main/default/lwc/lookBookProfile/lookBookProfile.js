e9c9ada81665de7e1bf2aac41225a4fb4f2ee22a
Jim Poteet <Jim Poteet>
2022-12-20 09:32:47 -0500

    Changed profile image - smaller size

--- force-app/main/default/lwc/lookBookProfile/lookBookProfile.js
+++ force-app/main/default/lwc/lookBookProfile/lookBookProfile.js
@@ -3,7 +3,7 @@ import LOOKBOOK_ASSETS from '@salesforce/resourceUrl/LookbookAssets';
 
 export default class LookBookProfile extends LightningElement {
      lookbookQuotes = LOOKBOOK_ASSETS + '/quotes.png';
-     lookbookProfilePic = LOOKBOOK_ASSETS + '/profile-pic-example.png';
+     lookbookProfilePic = LOOKBOOK_ASSETS + '/profile-pic-example.jpg';
      
      handleProfileClick() {
           this.dispatchEvent(new CustomEvent('profileclose'));
