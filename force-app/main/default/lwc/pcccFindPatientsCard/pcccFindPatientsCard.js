7964ed45f83da99d7fc4d0c824f004863eea5e39
Jim Poteet <Jim Poteet>
2022-12-22 13:55:03 -0500

    Fin patient component

--- force-app/main/default/lwc/pcccFindPatientsCard/pcccFindPatientsCard.js
+++ force-app/main/default/lwc/pcccFindPatientsCard/pcccFindPatientsCard.js
@@ -1,8 +1,16 @@
 import { LightningElement, api } from 'lwc';
+import LOOKBOOK_ASSETS from '@salesforce/resourceUrl/LookbookAssets';
 
 export default class PcccFindPatientsCard extends LightningElement {
+     
+     astroProfilePicSmall = LOOKBOOK_ASSETS + '/lightning_blue_profile_avatar_96.png';
+
      @api patientName;
      @api patientLocation;
      @api patientSelected;
      @api patientStatus;
+     @api showCheckbox;
+     @api showStatus;
+     @api showProfilePic;
+
 }
